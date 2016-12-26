7c6e38955396c2e934f07816c028f1c1a0b4a44e
alvinabhinav <alvin.abhinav@ithands.net>
2016-12-26 17:39:21 +0530

    custom js file updated

--- public/js/custom.js
+++ public/js/custom.js
@@ -9,26 +9,21 @@ $(document).ready(function(){
     $questions.hide();
     $($questions.get(currentQuestion)).fadeIn();
     $($comments.get(currentQuestion)).fadeIn();
-
     if(currentQuestion == 0) {
         $('.previous').fadeOut();
     }
     if(currentQuestion == totalQuestions - 1) {
         $('.next').fadeOut();
     }
-
     $('.previous').click(function(){
         updateQuestionScreen(false);
     });
-
     $('.next').click(function(){
         updateQuestionScreen(true);
     });
-
     function updateQuestionScreen(isNext) {
         $($questions.get(currentQuestion)).hide();
         $($comments.get(currentQuestion)).hide();
-
         var questionid=$($questions.get(currentQuestion)).find("input[name*='question']").val();
         var mandatory =$($questions.get(currentQuestion)).find("input[name*='mandatory']").val();
         var radioInline=$($questions.get(currentQuestion)).find("input[name*='radioInline']:checked").val();
@@ -37,7 +32,6 @@ $(document).ready(function(){
         var country=$($questions.get(currentQuestion)).find("input[name*='country']").val();
         var stage=$($questions.get(currentQuestion)).find("input[name*='stage']").val();
         var userid=$($questions.get(currentQuestion)).find("input[name*='userid']").val();
-
         if(radioInline === undefined) {
             radioInline = '';
         }
@@ -45,11 +39,9 @@ $(document).ready(function(){
             numberInline  = 0;
         }
         answers[currentQuestion] = questionid+'#'+radioInline+'#'+numberInline+'#'+notes+'#'+country+'#'+stage+'#'+userid;
-
         if(mandatory==1 && (radioInline==0 && numberInline == 0) && flagSave==1  ){
             flagSave=0;
         }
-
         if(currentQuestion == 1 && !isNext){
             $('.previous').hide();
             $('.next').fadeIn();
@@ -60,13 +52,11 @@ $(document).ready(function(){
             $('.next').fadeIn();
             $('.previous').fadeIn();
         }
-
         if(isNext) {
             currentQuestion++;
         } else {
             currentQuestion--;
         }
-
         if(currentQuestion == totalQuestions){
             $('.counter').hide();
             $.ajax({
@@ -87,7 +77,6 @@ $(document).ready(function(){
             $('#counter').text(currentQuestion+1);
         }
     }
-
     $('.save').click(function(){
         var questionid=$($questions.get(currentQuestion)).find("input[name*='question']").val();
         var mandatory =$($questions.get(currentQuestion)).find("input[name*='mandatory']").val();
@@ -103,7 +92,6 @@ $(document).ready(function(){
         if(numberInline === undefined) {
             numberInline  = 0;
         }
-
         answerstring = questionid+'#'+radioInline+'#'+numberInline+'#'+notes+'#'+country+'#'+stage+'#'+userid;
         $.ajax({
             url: "/movement/savenotes",
@@ -120,11 +108,7 @@ $(document).ready(function(){
                }
             }
         });
-
-
     });
-
-
     /*TOOLTIP*/
     $(function () {
         $('[data-toggle="tooltip"]').tooltip();
