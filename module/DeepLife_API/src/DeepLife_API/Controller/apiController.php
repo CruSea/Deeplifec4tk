60d3b413e5009d5b644dd85b2a099012a2203690
BENGEOS <george.beng@gmail.com>
2017-02-26 09:26:34 +0300

    remove all the print commit to track auth method

--- module/DeepLife_API/src/DeepLife_API/Controller/apiController.php
+++ module/DeepLife_API/src/DeepLife_API/Controller/apiController.php
@@ -294,14 +294,11 @@ class apiController extends AbstractRestfulController
         $smsService = $this->getServiceLocator()->get('DeepLife_API\Service\Service');
         $reqUserName = $data[Req::USER_NAME];
         $reqUserPass = $data[Req::USER_PASS];
-        print_r($data);
         if(isset($data[Req::PHONE_CODE])){
             /**
              * @var \DeepLife_API\Model\Country $PhoneCode
              */
-            print_r("Check: Get_Country_By_PhoneCode",$data[Req::PHONE_CODE]);
             $PhoneCode = $smsService->Get_Country_By_PhoneCode($data[Req::PHONE_CODE]);
-            print_r("Found PhoneCode: ",$PhoneCode);
             if($PhoneCode != null){
                 $reqCountry = $PhoneCode->getId();
             }else{
@@ -312,20 +309,14 @@ class apiController extends AbstractRestfulController
         }
         if(isset($reqUserName) && isset($reqUserPass) && isset($reqCountry)){
             if ($smsService->authenticate($reqUserName,$reqUserPass)) {
-                print_r("authenticate with email address");
                 $user = new User();
                 $user->setEmail($reqUserName);
-                print_r("Get User");
                 $this->api_User = $smsService->Get_User($user);
-                print_r("Get User done");
                 return true;
             }else if($smsService->authenticate2($reqUserName,$reqUserPass)){
-                print_r("authenticate with Phone address",$reqUserName);
                 $user = new User();
                 $user->setPhoneNo($reqUserName);
-                print_r("Get User");
                 $this->api_User = $smsService->Get_User($user);
-                print_r("Get User done");
                 if ($this->api_User->getCountry() == $reqCountry) {
                     return true;
                 }
